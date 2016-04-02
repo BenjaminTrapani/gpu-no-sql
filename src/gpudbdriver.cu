71d6793ddda8947cbb701a175cf36ae787184964
btrapani <btrapani@ccs.neu.edu>
2016-04-02 18:27:41 -0400

    Fast fully fleshed out querry.

--- src/gpudbdriver.cu
+++ src/gpudbdriver.cu
@@ -125,6 +125,7 @@ int main(int argc, char * argv[]){
     for(unsigned int i = 0; i < driver.getTableSize()-1; i++){
         Entry anEntry;
         anEntry.data.bigVal=0;
+        anEntry.valType = GPUDB_BGV;
         driver.create(anEntry);
     }
     Entry lastEntry;
