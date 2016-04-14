fad202d6c618f2539fbdc6cd8b1de2e4333a4813
adjkant <adjkant@gmail.com>
2016-04-14 19:38:54 -0400

    cleanup, small test changes, small data additions

---
+++ src/CoparatorType.h
@@ -0,0 +1,8 @@
+//
+// Created by Adrian Kant on 4/14/16.
+//
+
+#ifndef GPU_NO_SQL_COPARATORTYPE_H
+#define GPU_NO_SQL_COPARATORTYPE_H
+
+#endif //GPU_NO_SQL_COPARATORTYPE_H
---
+++ src/QueryResult.h
@@ -0,0 +1,8 @@
+//
+// Created by Adrian Kant on 4/14/16.
+//
+
+#ifndef GPU_NO_SQL_QUERYRESULT_H
+#define GPU_NO_SQL_QUERYRESULT_H
+
+#endif //GPU_NO_SQL_QUERYRESULT_H
