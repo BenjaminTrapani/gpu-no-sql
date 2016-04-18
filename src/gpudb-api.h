03b396df2c9bb25e2807bda3eaf8f53c3c383521
adjkant <adjkant@gmail.com>
2016-04-18 15:57:03 -0400

    basic documentation of API

--- src/gpudb-api.h
+++ src/gpudb-api.h
@@ -20,24 +20,34 @@ public:
     int getDoc(std::vector<std::string> strings);
 
     // Creation
+    // Returns the new Doc ID
     int newDoc(int docID, std::string key);
+    // Returns an error/success code
     int addToDoc(int docID, std::string & key, std::string & value, GPUDB_Type type);
+    // Returns an error/success code
     int batchAdd(int docID, std::vector<std::string> & keys, std::vector<std::string> & values, GPUDB_Type type);
-    bool commitDocTree(int docID);
+    // Returns an error/success code
+    int commitDocTree(int docID);
 
     // Filter Creation
+    // Returns the new filter ID on the given Doc
     int newFilter(int docID);
+    // Returns an error/success code
     int addToFilter(int filterID, std::vector<std::string> keys);
+    // Returns an error/success code
     int addToFilter(int filterID, std::vector<std::string> keys, std::string & value, GPUDB_COMP comp);
 
     // Querying
     GPUDB_QueryResult query(int filterID);
 
     // Updating - single filter only or error out
+    // Returns an error/success code
     int updateOnDoc(int filterID, std::string & value);
+    // Returns an error/success code
     int updateOnDoc(int filterID, std::string & value, GPUDB_Type type);
 
     // Deleting
+    // Returns an error/success code
     int deleteFromDoc(int filterID);
 private:
 
